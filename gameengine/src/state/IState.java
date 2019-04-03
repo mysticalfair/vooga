37cdd9013294886b57665b6be9d7d3224762d0ad
LUKE TRUITT <lot@duke.edu>
2019-04-03 12:36:28 -0400

    Issue #5, built State interfaces

--- gameengine/src/state/IState.java
+++ gameengine/src/state/IState.java
@@ -1,13 +1,19 @@
 package state;
 
+import state.Agent.IAgent;
+import state.Attribute.IAttribute;
+import state.Objective.IObjective;
+
 import java.util.List;
 
 /**
  * @author David Miron
  * @author Jamie Palka
+ * @Author:Luke_Truitt
  */
 public interface IState {
-    int getHealth();
-    List<IObjective> getObjectives();
+    List<IAgent> getImmutableOptions();
     List<IAgent> getImmutableAgents();
+    List<IObjective> getImmutableObjectives();
+    List<IAttribute> getImmutableAttributes();
 }

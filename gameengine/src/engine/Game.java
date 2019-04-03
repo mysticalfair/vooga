937d83b214975ccf1080e54996ab1f9f40284d2a
Jorge Raad <jar121@duke.edu>
2019-04-03 16:48:07 -0400

    #11 created game loop structure inside of Game that updates the state at the given rate

--- gameengine/src/engine/Game.java
+++ gameengine/src/engine/Game.java
@@ -3,15 +3,50 @@ package engine;
 import state.Agent.IAgentComplete;
 import state.Objective.IObjectiveComplete;
 import state.StateComplete;
-
+// Major Source: http://entropyinteractive.com/2011/02/game-engine-design-the-game-loop/
 /**
- * @Author:Luke_Truitt
+ * @Author Jorge Raad
+ * @Author Luke Truitt
  * @author David Miron
  * @author Jamie Palka
  */
 public class Game {
 
+    private boolean runFlag = false;
     private StateComplete state;
+
+    public void run(String gameFile, double deltaTime){
+        runFlag = true;
+        startup();
+        double nextTime = System.nanoTime()/1000000000.0;
+        while(runFlag){
+            double currentTime = System.nanoTime()/1000000000.0;
+
+            // if deltaTime has passed, then update
+            if(currentTime >= nextTime){
+                // assign time of next update
+                nextTime += deltaTime;
+                step();
+            }
+            else{
+                // TODO: may change according to how game engine interacts with player
+                // must convert from seconds to milliseconds
+                int sleepTime = 1000 * (int)(nextTime - currentTime);
+                //game loop should stop until it is time to update again
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    // TODO: handle this exception
+                }
+            }
+        }
+
+    }
+
+    private void startup() {
+        // TODO: deserialize a state from a file
+        // state = **
+    }
 
     private void step() {
 
@@ -30,6 +65,10 @@ public class Game {
      */
     private void sendState() {
         // TODO
+    }
+
+    public void stop(){
+        runFlag = false;
     }
 
 }

7bee2c254733977533c332b0e665bb2289c36ec9
LUKE TRUITT <lot@duke.edu>
2019-04-03 09:36:55 -0400

    Separated Agent by who has access

--- gameengine/src/state/IAgentComplete.java
+++ gameengine/src/state/IAgentComplete.java
@@ -3,12 +3,14 @@ package state;
 import java.util.List;
 
 /**
+ * @Author:Luke_Truitt
  * These are the extensions for the full agent. The one authoring and engine need.
  */
-public interface IEngineAgent extends IAgent{
-    void setCY();
-    String getImageURL();
-    Boolean getIsAttacking();
-    int getHealth();
+public interface IAgentComplete extends IAgent{
+    void setXY();
+    String setImageURL();
+    Boolean setIsAttacking();
+    int setHealth();
     void update(List<IAgent> agents);
+    IAgent toImmutable();
 }

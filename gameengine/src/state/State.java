52d47e3f55142e3c4c0c1da9947790038dd54f47
LUKE TRUITT <lot@duke.edu>
2019-04-03 09:38:13 -0400

    Built out State

--- gameengine/src/state/State.java
+++ gameengine/src/state/State.java
@@ -1,20 +1,28 @@
 package state;
 
+import java.util.ArrayList;
 import java.util.List;
-
-/**
- * @author David Miron
- * @author Jamie Palka
+ /**
+ * @Author:Luke_Truitt
  */
-public class State implements IStateAuthoring, IStatePlayer {
+public class State implements IStateComplete {
 
-    private List<IAgent> agentsOptions;
-    private List<IAgent> agentsCurrent;
+    private List<IAgentComplete> agentsOptions;
+    private List<IAgentComplete> agentsCurrent;
     private List<IObjective> objectivesCurrent;
+    private Map<IAttributes> attributesCurrent;
+    @Override
+    public List<IAgent> getImmutableAgents() {
+        List<IAgent> immutableAgentsCurrent = new ArrayList<>();
+        for ( IAgentComplete agent : this.agentsCurrent ) {
+            immutableAgentsCurrent.add(agent.toImmutable());
+        }
+        return immutableAgentsCurrent;
+    }
 
     @Override
-    public List<IAgent> getAgents() {
-        return null; // TODO
+    public List<IAgentComplete> getMutableAgents() {
+        return this.agentsCurrent;
     }
 
     @Override

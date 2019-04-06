8517a6741304b0f86a12366bfdb49c7fdd7543df
Jorge Raad <jar121@duke.edu>
2019-04-06 16:12:46 -0400

    simply made MovementAction extend Action

--- gameengine/src/state/action/movement/MovementAction.java
+++ gameengine/src/state/action/movement/MovementAction.java
@@ -1,10 +1,12 @@
 package state.action.movement;
 
+import state.action.Action;
+
 /**
  * Class to define how an agent should move
  * @author David Miron
  */
-public abstract class MovementAction {
+public abstract class MovementAction extends Action {
 
    protected int speed;
 

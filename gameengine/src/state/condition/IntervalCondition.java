80b6920384ebae5a244efb45f2b05e84edcf4255
David Miron <david.miron@duke.edu>
2019-04-08 16:27:47 -0400

    Issue #49 Create and implement IntervalCondition

---
+++ gameengine/src/state/condition/IntervalCondition.java
@@ -0,0 +1,41 @@
+package state.condition;
+
+import state.agent.IAgent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class to return valid agents based on some timing interval
+ * @author David Miron
+ */
+public class IntervalCondition implements ICondition {
+
+    private double intervalMillis;
+    private long lastOccurrence;
+
+    /**
+     * Create an IntervalCondition
+     * @param interval The interval, in seconds
+     */
+    public IntervalCondition(double interval) {
+        this.intervalMillis = interval * 1000;
+        this.lastOccurrence = 0;
+    }
+
+    /**
+     * Return all agents if some interval has passed, or else return none
+     * @param agents The agents to filter
+     * @return All the agents if some interval has passed, or else an empty list
+     */
+    @Override
+    public List<IAgent> getValid(List<IAgent> agents) {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastOccurrence > intervalMillis) {
+            lastOccurrence = currentTime;
+            return agents;
+        } else {
+            return new ArrayList<>();
+        }
+    }
+}

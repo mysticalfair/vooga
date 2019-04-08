918f971c527d9d374b9150f4aafbda66dc47bd63
Eric <el172@duke.edu>
2019-04-08 18:20:35 -0400

    refactored

--- src/authoring/src/panes/AuthoringEnvironment.java
+++ src/authoring/src/panes/AuthoringEnvironment.java
@@ -1,25 +1,13 @@
 package panes;
 
-import frontend_objects.AgentView;
 import frontend_objects.CloneableAgentView;
 import frontend_objects.DraggableAgentView;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.geometry.Point2D;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.util.List;
-import java.util.Map;
 
 public class AuthoringEnvironment extends Application {
 
@@ -32,10 +20,7 @@ public class AuthoringEnvironment extends Application {
     private ConsolePane consolePane;
     private AgentPane agentPane;
     private AttributesPane attributesPane;
-    private Pane handlePane;
     private MapPane map;
-    //private Canvas map;
-    private List<CloneableAgentView> agentInventory;
 
     public static void main(String[] args){
         launch(args);
@@ -46,12 +31,6 @@ public class AuthoringEnvironment extends Application {
         stackPane = new StackPane();
         borderPane = new BorderPane();
         stackPane.getChildren().add(borderPane);
-        handlePane = new Pane();
-        //map = new Canvas(250, 250);
-        //GraphicsContext gc = map.getGraphicsContext2D();
-        //gc.fillRect(0,0,400,400);
-        //gc.setFill(Color.BLACK);
-        //borderPane.setCenter(map);
         initAllPanes();
         initStage(stage);
     }
@@ -71,8 +50,7 @@ public class AuthoringEnvironment extends Application {
     private void initAgentPane() {
         agentPane = new AgentPane();
         agentPane.accessContainer(node -> borderPane.setRight(node));
-        agentInventory = agentPane.getAgentList();
-        for (CloneableAgentView o : agentInventory) {
+        for (CloneableAgentView o : agentPane.getAgentList()) {
             o.setOnMousePressed(e -> mousePressedOnClone(o));
         }
     }
@@ -119,17 +97,19 @@ public class AuthoringEnvironment extends Application {
 
     private void mouseReleased(DraggableAgentView view) {
         System.out.println(view.getTranslateX() + " " + view.getTranslateY());
-        if (view.getTranslateX() < 0 || rightOutOfBounds(view) ) {
+        if (outOfBounds(view)) {
             view.setImage(null);
             map.removeAgent(view);
         }
     }
 
-    private boolean rightOutOfBounds(DraggableAgentView view) {
+    private boolean outOfBounds(DraggableAgentView view) {
         double xPos = view.getTranslateX() + view.getFitWidth();
         double attributesWidth = attributesPane.getVBoxContainer().getWidth();
         double agentPanelWidth = agentPane.getContainerVBox().getWidth();
-        return (xPos > AuthoringEnvironment.DEFAULT_WIDTH - attributesWidth - agentPanelWidth);
+        boolean rightOutOfBounds = xPos > AuthoringEnvironment.DEFAULT_WIDTH - attributesWidth - agentPanelWidth;
+        boolean leftOutOfBounds = xPos < 0;
+        return leftOutOfBounds || rightOutOfBounds;
     }
 
     private void initStage(Stage stage) {

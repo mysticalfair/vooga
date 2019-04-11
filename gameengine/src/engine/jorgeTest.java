28942178a7ab14e328dd1b71b62352cd24cd1e13
Jorge Raad <jar121@duke.edu>
2019-04-10 21:36:41 -0400

    #14 creating test class to test serialization

--- gameengine/src/engine/jorgeTest.java
+++ gameengine/src/engine/jorgeTest.java
@@ -2,9 +2,34 @@ package engine;
 
 import state.IState;
 import state.State;
+import state.action.IAction;
+import state.action.PointToAgent;
+import state.actiondecision.ActionDecision;
+import state.agent.Agent;
+import state.condition.CollisionCondition;
+import state.condition.Condition;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class jorgeTest {
     public static void main(String[] args){
-        IState state = new State();
+
+        State state = new State();
+        Agent agent = new Agent(0, 0, 0, "Jorge", 50, 10, 10, 5, 90);
+
+        Agent agent2 = new Agent(0, 100, 100, "Jorge", 50, 10, 10, 0, 90);
+        IAction action = new PointToAgent();
+        List<Condition> conditions = new ArrayList<>();
+        conditions.add(new CollisionCondition(agent2));
+        ActionDecision actionDecision = new ActionDecision(action, conditions);
+
+        agent2.addActionDecisionRaw(actionDecision);
+
+        state.defineAgent(agent);
+        state.placeAgent(agent);
+        state.placeAgent(agent2);
+
+
     }
 }

9205a3bc3a208edef7baf8105c35e26d4ed28551
LUKE TRUITT <lot@duke.edu>
2019-04-15 22:02:58 -0400

    Property Object

---
+++ gameengine/src/state/Property.java
@@ -0,0 +1,29 @@
+package state;
+
+import gameengine.IPropertyDefinition;
+
+public class Property implements IPropertyDefinition {
+
+    private String name;
+    private Object value;
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Object getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}

4e01e5b0f94e54119c5693c9defa8c1b2514df49
Jorge Raad <jar121@duke.edu>
2019-04-15 22:15:27 -0400

    created test directory and created StateTest to test Serialization #55

---
+++ gameengine/test/state/StateTest.java
@@ -0,0 +1,12 @@
+package state;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}

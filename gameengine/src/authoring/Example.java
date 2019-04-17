d55fa31ff8a792930d0f6eee424613500a6a2802
David Miron <david.miron@duke.edu>
2019-04-17 01:29:57 -0400

    Add action decision creation to example

--- gameengine/src/authoring/Example.java
+++ gameengine/src/authoring/Example.java
@@ -1,5 +1,6 @@
 package authoring;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,10 +16,19 @@ public class Example {
 
             List<AvailableAction> actions = g.getAvailableActions();
 
+            Map<String, Object> aparams = new HashMap<>();
+            aparams.put("angle", 5.0);
+            aparams.put("speed", 3);
+            IActionDefinition actionDefinition = g.createAction("MoveAtRelativeAngle", aparams);
+
             Map<String, Object> cparams = new HashMap<>();
-            cparams.put("angle", 5.0);
-            cparams.put("speed", 3);
-            IActionDefinition actionDefinition = g.createAction("MoveAtRelativeAngle", cparams);
+            cparams.put("interval", 3.0);
+            IConditionDefinition conditionDefinition = g.createCondition("Interval", cparams);
+            List<IConditionDefinition> conditions = new ArrayList<>();
+            conditions.add(conditionDefinition);
+
+            IActionDecisionDefinition ad = g.createActionDecision(actionDefinition, conditions);
+
 
             System.out.println("Here");
         } catch (Exception e) {

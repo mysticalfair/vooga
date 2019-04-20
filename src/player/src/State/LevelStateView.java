a847e310339c3cba61119da0999d7235091320a3
LUKE TRUITT <lot@duke.edu>
2019-04-20 14:38:09 -0400

    LevelStateView added

--- src/player/src/State/LevelStateView.java
+++ src/player/src/State/LevelStateView.java
@@ -1,4 +1,32 @@
 package State;
 
-public class LevelStateView {
+import state.IPlayerLevelState;
+import state.agent.Agent;
+import state.agent.IPlayerAgent;
+import state.attribute.IAttribute;
+import state.objective.IPlayerObjective;
+import state.objective.Objective;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+public class LevelStateView implements PropertyChangeListener {
+    private List<IPlayerAgent> agentsOptions;
+    private List<IPlayerAgent> agentsCurrent;
+
+    public LevelStateView(IPlayerLevelState playerLevelState){
+        super();
+        this.init(playerLevelState);
+        playerLevelState.addPropertyChangeListener(this);
+    }
+
+    public void init(IPlayerLevelState playerLevelState){
+        this.agentsCurrent = playerLevelState.getImmutableAgents();
+        this.agentsOptions = playerLevelState.getImmutableOptions();
+    }
+
+    public void propertyChange(PropertyChangeEvent e) {
+        // TODO: Reflection to add or remove agents
+    }
 }

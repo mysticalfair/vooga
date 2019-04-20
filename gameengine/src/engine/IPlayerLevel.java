237a61cc1adab2e58f446aedfcb2451cd8c13316
LUKE TRUITT <lot@duke.edu>
2019-04-20 14:34:04 -0400

    IPlayer Level Added

--- gameengine/src/engine/IPlayerLevel.java
+++ gameengine/src/engine/IPlayerLevel.java
@@ -1,4 +1,10 @@
 package engine;
 
+import state.IPlayerLevelState;
+
+import java.beans.PropertyChangeListener;
+
 public interface IPlayerLevel {
+    IPlayerLevelState getLevelState();
+    void addPropertyChangeListener(PropertyChangeListener listener);
 }

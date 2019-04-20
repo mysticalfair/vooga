197fb23780734f9718aa06d2e225d7941c3abe1c
LUKE TRUITT <lot@duke.edu>
2019-04-20 14:36:27 -0400

    Adding Listener to PlayerAgent

--- gameengine/src/state/agent/PlayerAgent.java
+++ gameengine/src/state/agent/PlayerAgent.java
@@ -74,6 +74,7 @@ public class PlayerAgent implements IPlayerAgent, Serializable, Cloneable {
         var oldX = this.x;
         this.x = x;
         pcs.firePropertyChange("x", oldX, x);
+        System.out.println("changing x");
     }
 
     public void setY(double y){
@@ -124,6 +125,7 @@ public class PlayerAgent implements IPlayerAgent, Serializable, Cloneable {
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         this.pcs.addPropertyChangeListener(listener);
+        System.out.println("theres a listener");
     }
 
     @Override

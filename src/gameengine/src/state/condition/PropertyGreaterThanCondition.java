e573249f8f057c6dcf1f6e6fc7c1b213dc9333cd
LUKE TRUITT <lot@duke.edu>
2019-04-23 16:14:26 -0400

    Greater Than Condition

--- src/gameengine/src/state/condition/PropertyGreaterThanCondition.java
+++ src/gameengine/src/state/condition/PropertyGreaterThanCondition.java
@@ -4,14 +4,32 @@ import state.agent.Agent;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PropertyGreaterThanCondition extends Condition {
+    private Comparable value;
+    private String propertyName;
+
+    /**
+     * Create an IntervalCondition
+     */
     public PropertyGreaterThanCondition(Map<String, Object> params) {
         super(params);
     }
 
     @Override
+    public void setParams(Map<String, Object> params) {
+        this.propertyName = (String) params.get("property");
+        this.value = (Comparable) params.get("value");
+    }
+
+    /**
+     * Return all agents if some interval has passed, or else return none
+     * @param agents The agents to filter
+     * @return All the agents if some interval has passed, or else an empty list
+     */
+    @Override
     public List<Agent> getValid(List<Agent> agents) {
-        return null;
+        return agents.stream().filter(agent -> (this.value.compareTo(agent.getProperty(this.propertyName)) > 0)).collect(Collectors.toList());
     }
 }

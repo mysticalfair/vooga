63318509a0a1a17c277d485740d3f5252b929962
LUKE TRUITT <lot@duke.edu>
2019-04-23 16:17:22 -0400

    Less Than

--- src/gameengine/src/state/condition/PropertyLessThanCondition.java
+++ src/gameengine/src/state/condition/PropertyLessThanCondition.java
@@ -4,15 +4,33 @@ import state.agent.Agent;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PropertyLessThanCondition extends Condition {
 
+    private Comparable value;
+    private String propertyName;
+
+    /**
+     * Create an IntervalCondition
+     */
     public PropertyLessThanCondition(Map<String, Object> params) {
         super(params);
     }
 
     @Override
+    public void setParams(Map<String, Object> params) {
+        this.propertyName = (String) params.get("property");
+        this.value = (Comparable) params.get("value");
+    }
+
+    /**
+     * Return all agents if some interval has passed, or else return none
+     * @param agents The agents to filter
+     * @return All the agents if some interval has passed, or else an empty list
+     */
+    @Override
     public List<Agent> getValid(List<Agent> agents) {
-        return null;
+        return agents.stream().filter(agent -> (this.value.compareTo(agent.getProperty(this.propertyName)) < 0)).collect(Collectors.toList());
     }
 }

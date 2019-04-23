86e84421487f6d26e025ea95cae945f14dafb7be
LUKE TRUITT <lot@duke.edu>
2019-04-23 15:01:06 -0400

    Renaming Conditions

---
+++ src/gameengine/src/state/condition/PropertyEqualToCondition.java
@@ -0,0 +1,17 @@
+package state.condition;
+
+import state.agent.Agent;
+
+import java.util.List;
+import java.util.Map;
+
+public class PropertyEqualTo extends Condition {
+    public PropertyEqualTo(Map<String, Object> params) {
+        super(params);
+    }
+
+    @Override
+    public List<Agent> getValid(List<Agent> agents) {
+        return null;
+    }
+}

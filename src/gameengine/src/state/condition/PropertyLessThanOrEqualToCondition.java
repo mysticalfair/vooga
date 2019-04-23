a9637b58becba196dbd1d0492dafe95883209d67
LUKE TRUITT <lot@duke.edu>
2019-04-23 14:58:19 -0400

    Property Less Than Name Change

--- src/gameengine/src/state/condition/PropertyLessThanOrEqualToCondition.java
+++ src/gameengine/src/state/condition/PropertyLessThanOrEqualToCondition.java
@@ -1,4 +1,17 @@
 package state.condition;
 
-public class PropertyLessThanOrEqualToCondition {
+import state.agent.Agent;
+
+import java.util.List;
+import java.util.Map;
+
+public class PropertyLessThanOrEqualToCondition extends Condition {
+    public PropertyLessThanOrEqualToCondition(Map<String, Object> params) {
+        super(params);
+    }
+
+    @Override
+    public List<Agent> getValid(List<Agent> agents) {
+        return null;
+    }
 }

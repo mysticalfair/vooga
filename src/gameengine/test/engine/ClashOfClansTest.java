5efe619efecc371c480deb78e8bbe11ddf147d29
LUKE TRUITT <lot@duke.edu>
2019-04-28 15:20:32 -0400

    Test

--- src/gameengine/test/engine/ClashOfClansTest.java
+++ src/gameengine/test/engine/ClashOfClansTest.java
@@ -45,7 +45,7 @@ public class ClashOfClansTest {
 
             state.addDefinedAgent(createTownHall("home"));
 
-            level.addAgent("peashooter", 200, 200, 0, new ArrayList<Property>());
+            level.addAgent("home", 0, 0, 0, new ArrayList<>());
 
             state.addLevel(level);
             gameEngine.setState(state);
@@ -54,30 +54,13 @@ public class ClashOfClansTest {
             e.printStackTrace();
         }
     }
+
     private IAgentDefinition createTownHall(String name) throws ConditionDoesNotExistException, ReflectionException, ActionDoesNotExistException {
-        Map<String, Object> condParams = new HashMap<>();
-        condParams.put("property", "health");
-        condParams.put("value", 0.0);
-        Map<String, Object> actionParams = new HashMap<>();
-        Map<String, Object> action2Params = new HashMap<>();
-        action2Params.put("propertyName", "health");
-        action2Params.put("value", 5.0);
         List<IActionDecisionDefinition> actionDecisions = new ArrayList<>();
-        List<IConditionDefinition> conditionDefinitions = new ArrayList<>();
-        List<IConditionDefinition> cond1 = new ArrayList<>();
-        Map cond2Params = new HashMap();
-        cond2Params.put("interval", 5.0);
-        cond1.add(factory.createCondition("DoOnce", new HashMap()));
-        cond1.add(factory.createCondition("Interval", cond2Params));
-        conditionDefinitions.add(factory.createCondition("DoOnce", new HashMap<>()));
-        conditionDefinitions.add(factory.createCondition("PropertyLessThanOrEqualTo", condParams));
-        actionDecisions.add(factory.createActionDecision(
-                factory.createAction("DestroyAgent", actionParams), conditionDefinitions));
-        actionDecisions.add(factory.createActionDecision(factory.createAction("DecrementProperty", action2Params), cond1));
         List<IPropertyDefinition> properties = new ArrayList<>();
-        var prop = factory.createProperty("health", 10.0);
+        var prop = factory.createProperty("health", 100.0);
         properties.add(prop);
-        return factory.createAgent(50, 50, 10, 10 ,1, "Luke", "clash/ArcherQueen.png", actionDecisions, properties);
+        return factory.createAgent(0, 0, 20, 20 ,1, "home", "clash/TH10.png", actionDecisions, properties);
     }
 
 }

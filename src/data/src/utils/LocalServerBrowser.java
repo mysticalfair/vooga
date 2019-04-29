64cfb5ff510527dc97a96e357eb2d1c6f35ad148
Jacob Mullett <jtm51@duke.edu>
2019-04-29 01:36:09 -0400

    Changed server browser for #60 to be static

--- src/data/src/utils/LocalServerBrowser.java
+++ src/data/src/utils/LocalServerBrowser.java
@@ -8,23 +8,23 @@ public class LocalServerBrowser {
 
     private static String LOCAL_ADDR_PREFIX = "192.168.1.";
 
-    public List<String> scanForLocalServers(int expectedPort, Class serverInterface) {
+    public static List<String> scanForLocalServers(int expectedPort, Class serverInterface) {
         return IntStream.range(2,255)
                 .parallel()
                 .filter((addrEnd) -> {
             try {
-                Connectable connectable = NetworkFactory.buildClient(serverInterface, this, generateAddress(addrEnd), expectedPort);
+                Connectable connectable = NetworkFactory.buildClient(serverInterface, new Object(), generateAddress(addrEnd), expectedPort);
                 connectable.disconnect();
                 return true;
             } catch (Exception ex) {
                 return false;
             }
         })
-                .mapToObj(this::generateAddress)
+                .mapToObj(LocalServerBrowser::generateAddress)
                 .collect(Collectors.toList());
     }
 
-    private String generateAddress(int suffix) {
+    private static String generateAddress(int suffix) {
         return LOCAL_ADDR_PREFIX + suffix;
     }
 
